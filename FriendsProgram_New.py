649898a696d5fd3f2a880c3e8aa2aee9769cd4de
supercgeek <cameronbb3@gmail.com>
2015-08-01 21:43:03 -0400

    Case fix

--- FriendsProgram_New.py
+++ FriendsProgram_New.py
@@ -1,21 +1,23 @@
 #DATE: JULY 31, 2015
-#AUTHOR: KATHLEEN
+#AUTHOR: KATHLEENR
 #DESRIPTION: Pick your favorite character from friends and see a quote of what they said on the show
 
 question = raw_input("Who is your favorite character on friends?: ")
+question = question.lower(); #makes the case, no matter what it is, lowercase
+print question; #shows what ross is, you can comment this line out ;)
 
-if question == "Rachel":
+if question == "rachel": #changed all the if statements to lowercase versions
 	print "quote: I mean isnt that just kick-you-in-the-crotch, spit on your-neck-fantastic (case insensitive)"
 
-if question == "Ross":
+if question == "ross":
 	print "quote: We Were On A BREAK!!!"
 
-if question == "Monica":
+if question == "monica":
 	print "quote: Ive got this uncontrollable need to please people"
 
-if question == "Phoebe":
+if question == "phoebe":
 	print "quote: See? Hes your lobster!"
 
-if question == "Joey":
+if question == "joey":
 	print "quote: JOEY DOESNT SHARE FOOD"
 
