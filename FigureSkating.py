feffe208fcf8d243b1313138d7a88b1b9c2757e8
Kathleen Burgess <kbburgess2000@gmail.com>
2015-08-07 22:11:41 -0400

    Figure Skating

---
+++ FigureSkating.py
@@ -0,0 +1,30 @@
+#DATE: JULY 31, 2015
+#AUTHOR: KATHLEEN
+#DESRIPTION: This code will calculate your points based on the figure skating spins and jumps you exceute.
+
+#points system executed elements
+#jumps
+1A = 1.10
+2S =
+2T =
+2Lo =
+2F = 1.80
+2Lz = 
+2A = 2.10
+3S =
+3T =
+3Lo =
+3F =
+3Lz = 
+3A = 
+2Lz+2T+C = 3.40
+#spins
+LSpB = 1.20
+FSSpB = 1.70
+FCCoSpB = 1.70
+CCoSpB = 1.70
+
+calculate = raw_input("Which elements did you excecute in your program? ")
+calculate = calculate.lower()
+	print 
+ 
