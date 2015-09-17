cadbe038d0bb45bdf56a408846dc7308d89415a2
Kathleen Burgess <kbburgess2000@gmail.com>
2015-09-17 19:23:43 -0400

    This is for comp science class (gr.10)
    4.04 Create your own code.
    It tells you what your horoscope says about you.

---
+++ createyourowncode.py
@@ -0,0 +1,44 @@
+#Kathleen Burgess
+#September 17th, 2015
+#Grade 10 Computer science, Create your Own Code
+#This program will let you enter your horoscope sign and will output what your horoscope sign says about you!
+
+question = raw_input("Please enter your horoscope sign?:")
+
+if question == "cancer":
+	print "what your sign says about you: You are unique"
+
+if question == "virgo":
+	print "what your sign says about you: You make people smile"
+
+if question == "scorpio":
+	print "what your sign says about you: You brighten everyone's day"
+	
+if question == "aries":
+	print "what your sign says about you: You will do great things"
+	
+if question == "taurus":
+	print "what your sign says about you: You are one of a kind"
+	
+if question == "capricorn":
+	print "what your sign says about you: Your kindness is beautiful"
+	
+if question == "leo":
+	print "what your sign says about you: You are strong in ore than one way"
+	
+if question == "gemini":
+	print "what your sign says about you: You can do anything"
+	
+if question == "libra":
+	print "what your sign says about you: Your laugh is like music to everyone's ears"
+	
+if question == "sagittarius":
+	print "what your sign says about you: You lift people's spirit"
+	
+if question == "pisces":
+	print "what your sign says about you: You are important"
+	
+if question == "aquarius":
+	print "what your sign says about you: You are stronger than you think"
+
+
