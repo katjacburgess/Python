13f13ca42ec02816e345bd92e51cb093f076bf63
Kathleen Burgess <kbburgess2000@gmail.com>
2015-10-06 12:08:24 -0400

    Grade 10, Comp Science

---
+++ Assigment 3, Program 1.py
@@ -0,0 +1,15 @@
+#DATE: OCT 6TH, 2015
+#AUTHOR: KATHLEEN
+#Grade 10, Assignment 3, question 1
+#DESRIPTION:
+Num1 = int(raw_input("Please enter a number "))
+Num2 = int(raw_input("Please enter a second number "))
+
+if Num1 > Num2:
+	print "The first number is bigger than the second number you entered."
+	
+elif Num1 < Num2:
+	print "The first number is smallar than the second number entered."
+	
+#do you need a loop?
+#No you do not need a loop for this code because
